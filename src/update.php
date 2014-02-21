f4d47e7fe027704ac9920e88ba24f6cc7168b253
Joris Vandeweerd <joris@statik.be>
2014-02-21 12:21:53 +0100

    pallo to ride conversion

--- src/update.php
+++ src/update.php
@@ -4,6 +4,6 @@ if (!file_exists('application')) {
     mkdir('application');
 }
 
-if (file_exists('vendor/pallo/cli/src/cli.php')) {
-    copy('vendor/pallo/cli/src/cli.php', 'application/cli.php');
+if (file_exists('vendor/ride/cli/src/cli.php')) {
+    copy('vendor/ride/cli/src/cli.php', 'application/cli.php');
 }
